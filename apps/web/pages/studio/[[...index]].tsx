5181f9741bf762ac934af98a96a4ebaeeb3f2488
AriHoresh <78949378+AriHoresh@users.noreply.github.com>
2025-09-05 21:19:41 +0200

    fix(studio): move Sanity Studio to Pages Router

--- apps/web/pages/studio/[[...index]].tsx
+++ apps/web/pages/studio/[[...index]].tsx
@@ -1,9 +1,6 @@
-'use client'
-
+﻿'use client'
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../sanity.config'
-
 export default function StudioPage() {
-  return <NextStudio config={config} />
+return <NextStudio config={config} />
 }
-
