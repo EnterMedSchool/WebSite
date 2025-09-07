8fc0c29c8eaad219eab296bac7b29f149cf5a982
AriHoresh <78949378+AriHoresh@users.noreply.github.com>
2025-09-07 11:33:23 +0200

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh1.googleusercontent.com' },
+      { protocol: 'https', hostname: 'lh2.googleusercontent.com' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'lh4.googleusercontent.com' },
+      { protocol: 'https', hostname: 'lh5.googleusercontent.com' },
+      { protocol: 'https', hostname: 'lh6.googleusercontent.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'secure.gravatar.com' },
+    ],
+  },
+};
+
+module.exports = nextConfig;
+
